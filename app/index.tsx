8dbf68b7eda99e5e01bc378591011faf0bf74081
Boris Kudryavtsev <boriskk.work@gmail.com>
2019-08-11 00:02:21 -0700

    Add tsconfig.json

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
-import { render } from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 import App from './App'
 
-render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
