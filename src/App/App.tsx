fcb5a8fcf4c962657c2caf4c13b2a124bc043e56
Boris Kudryavtsev <boriskk.work@gmail.com>
2019-08-11 18:17:09 -0700

    Add babel, base component files

---
+++ src/App/App.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Charm from '../Charm/Charm'
+
+import './app.css'
+
+class App extends Component {
+  render() {
+    return(
+      <div className='container'>
+        <div className='content'>
+          
+        </div>
+
+        <Charm />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/Charm/Charm.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Charm extends Component {
+  render() {
+    return(
+      <div className='charm'>
+      </div>
+    )
+  }
+}
+
+export default Charm
